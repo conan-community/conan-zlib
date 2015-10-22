88fb43e1cc8dbb3b6b27071a449a223eeb21b3e6
Luis <lasote@gmail.com>
2015-10-22 12:27:41 +0200

    compiler.runtime for windows

--- build.py
+++ build.py
@@ -31,28 +31,28 @@ if __name__ == "__main__":
     if platform.system() == "Windows":
         compiler = '-s compiler="Visual Studio" -s compiler.version=12 '
         # Static x86
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s runtime=MDd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s runtime=MTd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s runtime=MD -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s runtime=MT -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
 
         # Static x86_64
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s runtime=MDd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s runtime=MTd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s runtime=MD -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s runtime=MT -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
 
         # Shared x86
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s runtime=MDd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s runtime=MTd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s runtime=MD -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s runtime=MT -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
 
         # Shared x86_64
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s runtime=MDd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s runtime=MTd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s runtime=MD -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s runtime=MT -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
+        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
 
     else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
         # Static x86
