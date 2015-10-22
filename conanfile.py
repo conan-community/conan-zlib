6a6fe2c32444d6acc04044cae4e230512ff47758
Luis Martinez <lasote@gmail.com>
2015-10-22 10:13:02 +0200

    First commit

---
+++ conanfile.py
@@ -0,0 +1,105 @@
+from conans import ConanFile
+import os
+from conans.tools import download
+from conans.tools import unzip
+from conans import CMake
+
+
+class ZlibConan(ConanFile):
+    name = "zlib"
+    version = "1.2.8"
+    ZIP_FOLDER_NAME = "zlib-1.2.8"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    exports = "CMakeLists.txt"
+
+    def conan_info(self):
+        # We don't want to change the package for each compiler version but
+        # we need the setting to compile with cmake
+        self.info.settings.compiler.version = "any"
+
+    def source(self):
+        zip_name = "zlib-1.2.8.zip"
+        download("http://zlib.net/zlib128.zip", zip_name)
+        unzip(zip_name)
+        os.unlink(zip_name)
+        if self.settings.os != "Windows":
+            self.run("chmod +x ./%s/configure" % self.ZIP_FOLDER_NAME)
+
+    def build(self):
+        """ Define your project building. You decide the way of building it
+            to reuse it later in any other project.
+        """
+        if self.settings.os == "Linux" or self.settings.os == "Macos":
+            self.run("cd %s &&  mkdir _build" % self.ZIP_FOLDER_NAME)
+            cd_build = "cd %s && cd _build" % self.ZIP_FOLDER_NAME
+            arch = "-m32 " if self.settings.arch == "x86" else ""
+            self.run("cd %s && CFLAGS='%s -mstackrealign -fPIC -O3' ./configure" % (self.ZIP_FOLDER_NAME, arch))
+            if self.settings.os == "Macos":
+                old_str = 'LDSHARED=gcc -dynamiclib -install_name ${exec_prefix}/lib/libz.1.dylib'
+                new_str = 'LDSHARED=gcc -dynamiclib -install_name libz.1.dylib'
+                replace_in_file("./%s/Makefile" % self.ZIP_FOLDER_NAME, old_str, new_str)
+            self.run("cd %s && make" % self.ZIP_FOLDER_NAME)
+        else:
+            cmake = CMake(self.settings)
+            if self.settings.os == "Windows":
+                self.run("IF not exist _build mkdir _build")
+            else:
+                self.run("mkdir _build")
+            cd_build = "cd _build"
+            self.output.warn('%s && cmake .. %s' % (cd_build, cmake.command_line))
+            self.run('%s && cmake .. %s' % (cd_build, cmake.command_line))
+            self.output.warn("%s && cmake --build . %s" % (cd_build, self.settings.build_config))
+            self.run("%s && cmake --build . %s" % (cd_build, self.settings.build_config))
+
+    def package(self):
+        """ Define your conan structure: headers, libs, bins and data. After building your
+            project, this method is called to create a defined structure:
+        """
+        # Copying zlib.h, zutil.h, zconf.h
+        self.copy("*.h", "include", "%s" % (self.ZIP_FOLDER_NAME), keep_path=False)
+        self.copy("*.h", "include", "%s" % ("_build"), keep_path=False)
+
+        # Copying static and dynamic libs
+        if self.settings.os == "Windows":
+            if self.options.shared:
+                self.copy(pattern="*.dll", dst="bin", src="_build", keep_path=False)
+                self.copy(pattern="*zlibd.lib", dst="lib", src="_build", keep_path=False)
+                self.copy(pattern="*zlib.lib", dst="lib", src="_build", keep_path=False)
+            else:
+                self.copy(pattern="*zlibstaticd.lib", dst="lib", src="_build", keep_path=False)
+                self.copy(pattern="*zlibstatic.lib", dst="lib", src="_build", keep_path=False)
+        else:
+            if self.options.shared:
+                if self.settings.os == "Macos":
+                    self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+                else:
+                    self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+            else:
+                self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.ZIP_FOLDER_NAME, keep_path=False)
+                self.copy(pattern="*.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+
+    def package_info(self):
+        if self.settings.os == "Windows":
+            if self.options.shared:
+                if self.settings.build_type == "Debug":
+                    self.cpp_info.libs = ['zlibd']
+                else:
+                    self.cpp_info.libs = ['zlib']
+            else:
+                if self.settings.build_type == "Debug":
+                    self.cpp_info.libs = ['zlibstaticd']
+                else:
+                    self.cpp_info.libs = ['zlibstatic']
+        else:
+            self.cpp_info.libs = ['z']
+
+
+def replace_in_file(file_path, search, replace):
+    with open(file_path, 'r') as content_file:
+        content = content_file.read()
+        content = content.replace(search, replace)
+    with open(file_path, 'wb') as handle:
+        handle.write(content)
---
+++ test/conanfile.py
@@ -0,0 +1,19 @@
+from conans.model.conan_file import ConanFile
+from conans import CMake
+
+
+class DefaultNameConan(ConanFile):
+    name = "DefaultName"
+    version = "0.1"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+    requires = "zlib/1.2.8@lasote/testing"
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake . %s' % cmake.command_line)
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
