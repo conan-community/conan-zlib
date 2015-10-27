ea138667cd3961175d648f8f8f77439a1113d241
Lasote <lasote@gmail.com>
2015-10-27 12:50:56 +0100

    Using conan test

--- build.py
+++ build.py
@@ -3,70 +3,55 @@ import hashlib
 import shutil
 import platform
 
-
-def system(command):
+    
+def test(arguments):
+    command = "conan test %s" % arguments
     retcode = os.system(command)
     if retcode != 0:
-        raise Exception("Error while executing:\n\t %s" % command)
-
-
-def build_run_example(settings):
-    current_dir = os.getcwd()
-    sha = hashlib.sha1(settings).hexdigest()
-    build_folder = os.path.join(current_dir, "conan_tmp", sha)
-    shutil.copytree("test", build_folder)
-    try:
-        os.chdir(build_folder)
-        system('conan install %s' % (settings))
-        system('conan build')
-        system("cd bin && .%senough" % (os.sep))
-    finally:
-        os.chdir(current_dir)
-
+        exit("Error while executing:\n\t %s" % command)
 
 if __name__ == "__main__":
-    system('conan export lasote/stable')
+    os.system('conan export lasote/stable')
 
-    shutil.rmtree("conan_tmp", ignore_errors=True)
     if platform.system() == "Windows":
         compiler = '-s compiler="Visual Studio" -s compiler.version=12 '
         # Static x86
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
 
         # Static x86_64
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=False')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=False')
 
         # Shared x86
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
+        test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
 
         # Shared x86_64
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
-        build_run_example(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o zlib:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o zlib:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o zlib:shared=True')
+        test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
 
     else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
         # Static x86
-        build_run_example('-s arch=x86 -s build_type=Debug -o zlib:shared=False')
-        build_run_example('-s arch=x86 -s build_type=Release -o zlib:shared=False')
+        test('-s arch=x86 -s build_type=Debug -o zlib:shared=False')
+        test('-s arch=x86 -s build_type=Release -o zlib:shared=False')
 
         # Shared x86
-        build_run_example('-s arch=x86 -s build_type=Debug -o zlib:shared=True')
-        build_run_example('-s arch=x86 -s build_type=Release -o zlib:shared=True')
+        test('-s arch=x86 -s build_type=Debug -o zlib:shared=True')
+        test('-s arch=x86 -s build_type=Release -o zlib:shared=True')
 
         # Static x86_64
-        build_run_example('-s arch=x86_64 -s build_type=Debug -o zlib:shared=False')
-        build_run_example('-s arch=x86_64 -s build_type=Release -o zlib:shared=False')
+        test('-s arch=x86_64 -s build_type=Debug -o zlib:shared=False')
+        test('-s arch=x86_64 -s build_type=Release -o zlib:shared=False')
 
         # Shared x86_64
-        build_run_example('-s arch=x86_64 -s build_type=Debug -o zlib:shared=True')
-        build_run_example('-s arch=x86_64 -s build_type=Release -o zlib:shared=True')
+        test('-s arch=x86_64 -s build_type=Debug -o zlib:shared=True')
+        test('-s arch=x86_64 -s build_type=Release -o zlib:shared=True')
