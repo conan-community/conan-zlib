ccf929b90fa0dcb84f1171e1534491e0130e42b7
Lasote <lasote@gmail.com>
2015-11-03 15:42:13 +0100

    better travis

--- build.py
+++ build.py
@@ -1,17 +1,17 @@
 import os
-import hashlib
-import shutil
 import platform
-
-    
-def test(arguments):
-    command = "conan test %s" % arguments
-    retcode = os.system(command)
-    if retcode != 0:
-        exit("Error while executing:\n\t %s" % command)
+import sys
 
 if __name__ == "__main__":
     os.system('conan export lasote/stable')
+   
+    def test(settings):
+        argv =  " ".join(sys.argv[1:])
+        command = "conan test %s %s" % (settings, argv)
+        retcode = os.system(command)
+        if retcode != 0:
+            exit("Error while executing:\n\t %s" % command)
+
 
     if platform.system() == "Windows":
         compiler = '-s compiler="Visual Studio" -s compiler.version=12 '
@@ -40,13 +40,14 @@ if __name__ == "__main__":
         test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o zlib:shared=True')
 
     else:  # Compiler and version not specified, please set it in your home/.conan/conan.conf (Valid for Macos and Linux)
-        # Static x86
-        test('-s arch=x86 -s build_type=Debug -o zlib:shared=False')
-        test('-s arch=x86 -s build_type=Release -o zlib:shared=False')
-
-        # Shared x86
-        test('-s arch=x86 -s build_type=Debug -o zlib:shared=True')
-        test('-s arch=x86 -s build_type=Release -o zlib:shared=True')
+        if not os.getenv("TRAVIS", False):  
+            # Static x86
+            test('-s arch=x86 -s build_type=Debug -o zlib:shared=False')
+            test('-s arch=x86 -s build_type=Release -o zlib:shared=False')
+    
+            # Shared x86
+            test('-s arch=x86 -s build_type=Debug -o zlib:shared=True')
+            test('-s arch=x86 -s build_type=Release -o zlib:shared=True')
 
         # Static x86_64
         test('-s arch=x86_64 -s build_type=Debug -o zlib:shared=False')
