afd1dbf395f3d169b9a8308d4312e7af4a82b6ec
Luis Martinez de Bartolome <lasote@gmail.com>
2016-01-14 19:20:00 +0100

    Try a docker travis build

--- build_with_docker.py
+++ build_with_docker.py
@@ -3,12 +3,17 @@ import platform
 import sys
 
 if __name__ == "__main__":
+
+    if len(sys.argv)==2:
+         versions = [sys.argv[1]]
+    else:
+         versions = ["4.6", "4.8", "4.9", "5.2", "5.3"]
         
-    for gcc_version in ["4.6", "4.8", "4.9", "5.2"]:
+    for gcc_version in versions:
         image_name = "lasote/conangcc%s" % gcc_version.replace(".", "")
         os.system("sudo docker pull %s" % image_name)
         curdir = os.path.abspath(os.path.curdir)
         command = 'sudo docker run --rm  -v %s:/home/conan/project -v '\
-                  '~/.conan/data:/home/conan/.conan/data -i %s /bin/sh -c '\
-                  '"cd project && python build.py"' % (curdir, image_name)
+                  '~/.conan/data:/home/conan/.conan/data -it %s /bin/sh -c '\
+                  '"cd project && sudo pip install conan --upgrade && python build.py"' % (curdir, image_name)
         os.system(command)
