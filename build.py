558f1da78848f49ded280e9f061507a172656af5
Lasote <lasote@gmail.com>
2016-01-22 18:44:13 +0100

    Try with auto generated combinations

--- build.py
+++ build.py
@@ -3,32 +3,7 @@ from conan.packager import ConanMultiPackager
 import sys
 import platform
 from copy import copy
-
-def add_visual_builds(builder, visual_version, arch):
-
-    base_set = {"compiler": "Visual Studio", 
-                "compiler.version": visual_version, 
-                "arch": arch}
-    sets = []
-    sets.append([{"build_type": "Release", "compiler.runtime": "MT"}, {"zlib:shared":False}])        
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MTd"}, {"zlib:shared":False}])
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MDd"}, {"zlib:shared":False}])
-    sets.append([{"build_type": "Release", "compiler.runtime": "MD"}, {"zlib:shared":False}])
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MDd"}, {"zlib:shared":True}])
-    sets.append([{"build_type": "Release", "compiler.runtime": "MD"}, {"zlib:shared":True}])        
       
-    for setting, options in sets:
-       tmp = copy(base_set)
-       tmp.update(setting)
-       builder.add(tmp, options)
-       
-def add_other_builds(builder):
-    # Not specified compiler or compiler version, will use the auto detected     
-    for arch in ["x86", "x86_64"]:
-        for shared in [True, False]:
-            for build_type in ["Debug", "Release"]:
-                builder.add({"arch":arch, "build_type": build_type}, {"zlib:shared": shared})
-           
 
 if __name__ == "__main__":
     channel = os.getenv("CONAN_CHANNEL", "testing")
@@ -62,12 +37,8 @@ if __name__ == "__main__":
     
     args = " ".join(sys.argv[1:])
     builder = ConanMultiPackager(args, username, channel)
-    if platform.system() == "Windows":
-        for visual_version in [10, 12, 14]:
-            for arch in ["x86", "x86_64"]:
-                add_visual_builds(builder, visual_version, arch)
-    else:
-        add_other_builds(builder)
+    builder.add_common_builds(package_name="zlib", shared_option_name="shared")
+    print(builder.builds)
     
     if use_docker:  
         builder.docker_pack(current_page, total_pages, gcc_versions)
