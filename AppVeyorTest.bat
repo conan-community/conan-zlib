21be6787db9e669cc308f680b6cfb0ea6f62abfb
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-07-05 15:03:14 +0200

    where conan

--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -1,9 +1,10 @@
 echo on
 where cmake
+where conan
 
 if "%CONAN_COMPILER%" equ "" goto conan_package_tools
 conan export lasote/testing
-set path=c:\mingw\bin&&conan test_package . -s arch="x86" -s build_type="Release" -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version="4.8" -o zlib:shared="False"
+set path=C:\Program Files (x86)\CMake\bin;c:\mingw\bin&&conan test_package . -s arch="x86" -s build_type="Release" -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version="4.8" -o zlib:shared="False"
 goto :eof
 
 :conan_package_tools
