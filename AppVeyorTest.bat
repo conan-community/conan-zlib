ab6f72a645727ab0587ddac9f359cce8896a874d
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-07-05 13:50:32 +0200

    small improvements

--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -1,7 +1,7 @@
 if "%CONAN_COMPILER%" equ "" goto conan_package_tools
-set cc=c:\mingw32\bin\gcc.exe
+set cc=c:\mingw\bin\gcc.exe
 conan export lasote/testing
-conan test_package
+conan test_package . -s arch="x86" -s build_type="Release" -s compiler=gcc -s compiler.version="4.8" -o zlib:shared="False"
 goto :eof
 
 :conan_package_tools
