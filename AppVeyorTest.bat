5a8b24b22d021031b474e5c6877a9fa7cfee74e3
Tim Lebedkov <tim.lebedkov@gmail.com>
2016-07-08 14:38:08 +0200

    C:

--- AppVeyorTest.bat
+++ AppVeyorTest.bat
@@ -4,7 +4,7 @@ where conan
 
 if "%CONAN_COMPILER%" equ "" goto conan_package_tools
 conan export lasote/ci
-set path=C:\Python27\Scripts;C:\Program Files (x86)\CMake\bin;c:\c:\mingw-w64-i686-sjlj-posix-4.9.2\bin&&conan test_package -s os="Windows" -s arch="x86" -s build_type="Release" -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version="4.9" -o zlib:shared="True"
+set path=C:\Python27\Scripts;C:\Program Files (x86)\CMake\bin;c:\mingw-w64-i686-sjlj-posix-4.9.2\bin&&conan test_package -s os="Windows" -s arch="x86" -s build_type="Release" -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version="4.9" -o zlib:shared="True"
 goto :eof
 
 :conan_package_tools
