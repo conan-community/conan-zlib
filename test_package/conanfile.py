db0a86b0ae309f046a280dee26ebba8ba12f023c
Lasote <lasote@gmail.com>
2017-01-19 10:26:53 +0100

    Quotes on cmake dir

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -19,7 +19,7 @@ class DefaultNameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
