9cdcbbc7254a19bc7cf48d87c0c1a5c58a07e3a8
ClausKlein <claus.klein@arcormail.de>
2018-04-06 00:25:35 +0200

    keep it simple as possible, use cmake

--- conanfile.py
+++ conanfile.py
@@ -26,34 +26,23 @@ class ZlibConan(ConanFile):
         tools.unzip(z_name)
         os.unlink(z_name)
         files.rmdir("%s/contrib" % self.ZIP_FOLDER_NAME)
-        if self.settings.os != "Windows":
-            self.run("chmod +x ./%s/configure" % self.ZIP_FOLDER_NAME)
+        # if self.settings.os != "Windows":
+        ### self.run("chmod +x ./%s/configure" % self.ZIP_FOLDER_NAME)
 
     def build(self):
         with tools.chdir(os.path.join(self.source_folder, self.ZIP_FOLDER_NAME)):
             files.mkdir("_build")
             with tools.chdir("_build"):
-                if not tools.os_info.is_windows:
-                    env_build = AutoToolsBuildEnvironment(self)
-                    if self.settings.arch in ["x86", "x86_64"] and self.settings.compiler in ["apple-clang", "clang", "gcc"]:
-                        env_build.flags.append('-mstackrealign')
-
-                    env_build.fpic = True
-
-                    if self.settings.os == "Macos":
-                        old_str = '-install_name $libdir/$SHAREDLIBM'
-                        new_str = '-install_name $SHAREDLIBM'
-                        tools.replace_in_file("../configure", old_str, new_str)
-
-                    if self.settings.os == "Windows":  # Cross building to Linux
-                        tools.replace_in_file("../configure", 'LDSHAREDLIBC="${LDSHAREDLIBC--lc}"', 'LDSHAREDLIBC=""')
-                    # Zlib configure doesnt allow this parameters
-                    env_build.configure("../", build=False, host=False, target=False)
-                    env_build.make()
+                cmake = CMake(self)
+                if self.options.shared:
+                    definitions = {
+                        "BUILD_SHARED_LIBS": "ON"
+                    }
+                    cmake.configure(defs=definitions, build_dir=".")
                 else:
-                    cmake = CMake(self)
                     cmake.configure(build_dir=".")
-                    cmake.build(build_dir=".")
+                cmake.build(build_dir=".")
+                cmake.install()
 
     def package(self):
         self.output.warn("local cache: %s" % self.in_local_cache)
@@ -67,48 +56,12 @@ class ZlibConan(ConanFile):
         # Copy the license files
         self.copy("LICENSE", src=self.ZIP_FOLDER_NAME, dst=".")
 
+        # NOTE: done by cmake.install()
         # Copy pc file
-        self.copy("*.pc", dst="", keep_path=False)
+        ### self.copy("*.pc", dst="", keep_path=False)
         # Copying zlib.h, zutil.h, zconf.h
-        self.copy("*.h", "include", "%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("*.h", "include", "%s" % "_build", keep_path=False)
-
-        # Copying static and dynamic libs
-        build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
-        if self.settings.os == "Windows":
-            if self.options.shared:
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
-                self.copy(pattern="*.dll", dst="bin", src=build_dir, keep_path=False)
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build/lib")
-                self.copy(pattern="*zlibd.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.dll.a", dst="lib", src=build_dir, keep_path=False)
-            else:
-                build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build/lib")
-                if self.settings.os == "Windows":
-                    # MinGW
-                    self.copy(pattern="libzlibstaticd.a", dst="lib", src=build_dir, keep_path=False)
-                    self.copy(pattern="libzlibstatic.a", dst="lib", src=build_dir, keep_path=False)
-                    # Visual Studio
-                    self.copy(pattern="zlibstaticd.lib", dst="lib", src=build_dir, keep_path=False)
-                    self.copy(pattern="zlibstatic.lib", dst="lib", src=build_dir, keep_path=False)
-
-                lib_path = os.path.join(self.package_folder, "lib")
-                suffix = "d" if self.settings.build_type == "Debug" else ""
-                if self.settings.compiler == "Visual Studio":
-                    current_lib = os.path.join(lib_path, "zlibstatic%s.lib" % suffix)
-                    os.rename(current_lib, os.path.join(lib_path, "zlib%s.lib" % suffix))
-                elif self.settings.compiler == "gcc":
-                    current_lib = os.path.join(lib_path, "libzlibstatic.a")
-                    os.rename(current_lib, os.path.join(lib_path, "libzlib.a"))
-        else:
-            if self.options.shared:
-                if self.settings.os == "Macos":
-                    self.copy(pattern="*.dylib", dst="lib", src=build_dir, keep_path=False)
-                else:
-                    self.copy(pattern="*.so*", dst="lib", src=build_dir, keep_path=False)
-            else:
-                self.copy(pattern="*.a", dst="lib", src=build_dir, keep_path=False)
+        ### self.copy("*.h", "include", "%s" % self.ZIP_FOLDER_NAME, keep_path=False)
+        ### self.copy("*.h", "include", "%s" % "_build", keep_path=False)
 
     def package_info(self):
         if self.settings.os == "Windows":
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,6 +1,7 @@
 from conans.model.conan_file import ConanFile, tools
 from conans import CMake
-import os, platform
+import os
+import platform
 
 
 class DefaultNameConan(ConanFile):
@@ -10,18 +11,15 @@ class DefaultNameConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        # Compatibility with Conan < 1.0 (#2234)
-        generator = "Unix Makefiles" if platform.system() != "Windows" else None 
-        cmake = CMake(self, generator=generator)
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
         self.copy(pattern="*.dylib", dst="bin", src="lib")
-        
+
     def test(self):
         if not tools.cross_building(self.settings):
             self.run("cd bin && .%senough" % os.sep)
         assert os.path.exists(os.path.join(self.deps_cpp_info["zlib"].rootpath, "LICENSE"))
-
