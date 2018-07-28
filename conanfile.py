1eb92e03a56db95268cbeccce96720750d7bad1c
SSE4 <tomskside@gmail.com>
2018-07-28 23:43:51 +0700

    - more correct check for compiler definition
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -34,10 +34,10 @@ class ZlibConan(ConanFile):
             for filename in ['zconf.h', 'zconf.h.cmakein', 'zconf.h.in']:
                 tools.replace_in_file(filename,
                                       '#ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */',
-                                      '#if defined(HAVE_UNISTD_H) && HAVE_UNISTD_H != 0')
+                                      '#if defined(HAVE_UNISTD_H) && (1-HAVE_UNISTD_H-1 != 0)')
                 tools.replace_in_file(filename,
                                       '#ifdef HAVE_STDARG_H    /* may be set to #if 1 by ./configure */',
-                                      '#if defined(HAVE_STDARG_H) && HAVE_STDARG_H != 0')
+                                      '#if defined(HAVE_STDARG_H) && (1-HAVE_STDARG_H-1 != 0)')
             files.mkdir("_build")
             with tools.chdir("_build"):
                 if not tools.os_info.is_windows:
