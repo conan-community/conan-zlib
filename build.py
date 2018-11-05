3db5e6e1ab99979a0210a0572ad877f6e0a67dce
Uilian Ries <uilianries@gmail.com>
2018-11-05 09:50:00 -0200

    #99 Apply Conan convention
    - Update recipe with Conan conventions
    - Add fPIC option
    - Update test package using deflate example
    - Run python 3.7 by default
    - Fix license file path
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
-from conan.packager import ConanMultiPackager
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from cpt.packager import ConanMultiPackager
+
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="zlib:shared", pure_c=True)
+    builder.add_common_builds(pure_c=True)
     builder.run()
