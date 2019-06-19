9fd2d126ad45a5b15dcb7bdeb41d752b008133c3
Julien Marrec <julien.marrec@gmail.com>
2019-06-19 11:11:10 +0200

    Fix FindZLIB.cmake (done before changing the test). This file can be removed actually

--- FindZLIB.cmake
+++ FindZLIB.cmake
@@ -3,11 +3,14 @@ if(ZLIB_FOUND)
 endif()
 
 find_path(ZLIB_INCLUDE_DIR NAMES zlib.h PATHS ${CONAN_INCLUDE_DIRS_ZLIB})
-find_library(ZLIB_LIBRARY NAMES ${CONAN_LIBS_ZLIB} PATHS ${CONAN_LIB_DIRS_ZLIB})
+
+foreach(conan_lib_zlib ${CONAN_LIBS_ZLIB})
+  find_library(ZLIB_LIB_${conan_lib_zlib} NAMES ${conan_lib_zlib} PATHS ${CONAN_LIB_DIRS_ZLIB})
+  list(APPEND ZLIB_LIBRARIES ${ZLIB_LIB_${conan_lib_zlib}})
+endforeach()
 
 set(ZLIB_FOUND TRUE)
 set(ZLIB_INCLUDE_DIRS ${ZLIB_INCLUDE_DIR})
-set(ZLIB_LIBRARIES ${ZLIB_LIBRARY})
 mark_as_advanced(ZLIB_LIBRARIES ZLIB_LIBRARY ZLIB_INCLUDE_DIR)
 
 if (NOT TARGET ZLIB::ZLIB)
